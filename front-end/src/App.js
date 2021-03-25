4bda749b9079a910dd59126bf627c46e930346cb
AKAaayush <shresthaaayush81@gmail.com>
2021-03-25 12:55:08 +0545

    HomePage

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,21 +1,23 @@
 
 //import './App.css';
-import Header from './components/Header'
-import Container from './Routes'
-import Footer from './components/Footer'
-import 'bootstrap/dist/css/bootstrap.min.css'
+//import Header from './components/Header'
+import Route from './Routes'
+//import Footer from './components/Footer'
+
+//import 'bootstrap/dist/css/bootstrap.min.css'
 import {BrowserRouter} from 'react-router-dom'
 
 function App() {
  
   return (
     <BrowserRouter>
-    <div className="App">
+    <Route/>
+    {/* <div className="App">
       <Header></Header>
       <Container></Container>
       <Footer></Footer>
 
-    </div>
+    </div> */}
     </BrowserRouter>
   );
 }
--- front-end/src/Routes.js
+++ front-end/src/Routes.js
@@ -4,20 +4,23 @@ import Login from './container/Login'
 import MenuAdd from './container/MenuAdd'
 import MyMenu from './container/MyMenu'
 import UpdateMenu from './container/UpdateMenu'
-
+import Header from './components/Header'
+import Footer from './components/Footer'
 import {Route} from 'react-router-dom'
 
  class Routes extends Component {
      render(){
          return(
         
-            <div>
+            <div className="animsition" style={{ opacity: '1' }}>
+                <Header/>
+                
             <Route path = '/menuadd' component={MenuAdd}></Route>
             <Route path = '/register' component={Register}></Route>
             <Route path = '/mymenu' component={MyMenu}></Route>
             <Route path = '/updatemenu/:id' component={UpdateMenu}></Route>
             <Route path = '/login' component = {Login}></Route>
-            
+            <Footer/>
             </div>
           
          
